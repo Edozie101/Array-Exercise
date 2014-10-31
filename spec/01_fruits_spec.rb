a81b432b08798e4392b256dde7b6f3792812410b
edzye101 <edzye101@gmail.com>
2014-10-31 11:19:57 -0700

    finishing touches to multiple changes

--- spec/01_fruits_spec.rb
+++ spec/01_fruits_spec.rb
@@ -5,33 +5,33 @@ describe 'Array basics' do
   let(:fruit) { ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes'] }
 
   it "can report its length" do
-    fruit_length = ___
+    fruit_length = fruit.size
     expect( fruit_length ).to be == 5
   end
 
   xit "can report the index of the string 'banana'" do
-    banana_index = ___
+    banana_index = fruit.index("banana")
     expect( banana_index ).to be == 2
   end
 
   xit "can create a new array where 'raspberries' is between apples and oranges" do
-    new_array = ___
+    new_array = fruit.dup.insert(1, 'raspberries')
     expect( new_array ).to be == ['apples', 'raspberries', 'oranges', 'bananas', 'pomegranates', 'grapes']
     expect(fruits.count).to be == 5
   end
 
   xit "can report the length of each string in the array" do
-    lengths = ___
+    lengths = fruit.dup.insert(1,'raspberries').map{|n| n.length}
     expect( lengths ).to be == [6, 11, 7, 7, 12, 6]
   end
 
   xit "can create a new array where all the fruit capitalized" do
-    new_array = ___
+    new_array = fruit.dup.insert(1,'raspberries').map{ |n| n.upcase}
     expect( new_array ).to be == ["APPLES", "RASPBERRIES", "ORANGES", "BANANAS", "POMEGRANATES", "GRAPES"]
   end
 
   xit "can create a new array that returns only fruits containing a 'g'" do
-    new_array = ___
+    new_array = fruit.delete_if{|n| !(include?"g") }
     expect( new_array ).to be == ["oranges", "pomegranates", "grapes"]
   end
 end
--- spec/04_tic_tac_toe_spec.rb
+++ spec/04_tic_tac_toe_spec.rb
@@ -17,31 +17,74 @@ describe "Tic Tac Toe, in arrays" do
 
   describe "counting usage per row" do
     it "returns how many times X was played in each row" do
-      xs_per_row = ___
+      xs_per_row = data.map{|row| row.count("O")}
       expect(xs_per_row).to be == [1, 2, 1]
     end
 
     xit "returns how many times O was played in each row" do
-      os_per_row = ___
+      os_per_row = data.map{ |row| row.count("O")}
       expect(os_per_row).to be == [2, 1, 2]
     end
   end
 
   describe "getting coordinates of usage" do
     xit "returns an array of [row, column] array coordinates for each usage of X" do
-      x_coordinates = ___
+      x_coordinates = []
+      row_index = 0
+      column_index = 0
+      data.each do |row|
+        row.each do |column|
+          if column == "X"
+            x_coordinates << [row_index, column_index]
+          end
+          column_index += 1
+        end
+        column_index = 0
+        row_index += 1
+      end
       expect(x_coordinates).to be == [[0, 0], [1, 0], [1, 1], [2, 1]]
     end
 
     xit "returns an array of [row, column] array coordinates for each usage of O" do
-      x_coordinates = ___
+      x_coordinates = x_coordinates = []
+      row_index = 0
+      column_index = 0
+      data.each do |row|
+        row.each do |column|
+          if column == "O"
+            x_coordinates << [row_index, column_index]
+          end
+          column_index += 1
+        end
+        column_index = 0
+        row_index += 1
+      end
       expect(x_coordinates).to be == [[0, 1], [0, 2], [1, 2], [2, 0], [2, 2]]
     end
   end
 
   describe "testing who won" do
     xit "determines whether X or O won" do
-      winner = ___
+      winner =
+       # just trying out functions and the .ord method 'discovered' by the russian dude
+      # to create a unique "winning number"
+      # method is totally unneccesary but still enjoy trying it out
+
+        def sum(datablock)
+          agg = 0
+          datablock.each_char { |a| agg += a.ord}
+          return agg
+        end
+        winner=''
+        if sum(data.join[0..2]) || sum(data.join[3..5]) || sum(data.join[6..8]) == 237 #"OOO"
+          winner = "O"
+        elsif   sum(data.join[0..2]) || sum(data.join[3..5])  || sum(data.join[6..8]) == 264    #"XXX"
+          winner = "X"
+
+      end
+
+      p winner
+
       expect(winner).to be == 'O'
     end
   end
